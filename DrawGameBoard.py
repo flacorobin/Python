503af307b1d3eb776e5838cf2d88b49cdaab6d86
Robinson Rodriguez <flacorobin@gmail.com>
2019-10-17 17:00:33 -0400

    Added DrawGameBoard and TicTacToe v1

---
+++ DrawGameBoard.py
@@ -0,0 +1,14 @@
+#https://www.practicepython.org/exercise/2014/12/27/24-draw-a-game-board.html
+#Ask the user what size game board they want to draw, and draw it for them to the screen using Python’s print statement.
+
+def print_board(x=3, y=3):
+    top = " ---"
+    side = "|   "
+
+    for i in range(y):
+        print(top * x + ' ')
+        print(side * x + '|')
+
+    print(top * x + ' ') #Printing bottom of the board
+
+print_board(3,3)
